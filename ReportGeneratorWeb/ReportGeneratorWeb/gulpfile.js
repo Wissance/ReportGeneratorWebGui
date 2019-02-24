77a7a4adc3f4ea07612672a0d6f7a543a552a6fc
Ushakov Michael <um.nix.user@gmail.com>
2019-02-24 22:57:52 +0300

    gulp uses nodescripts

--- ReportGeneratorWeb/ReportGeneratorWeb/gulpfile.js
+++ ReportGeneratorWeb/ReportGeneratorWeb/gulpfile.js
@@ -8,15 +8,28 @@ var gulp = require("gulp"),
     uglify = require("gulp-uglify");
 
 var paths = {
-    webroot: "./wwwroot/"
+    webroot: "./wwwroot/",
+    nodeModules: "./node_modules"
 };
+
+var nodeCss = [
+    // bootstrap
+    paths.nodeModules + "/bootstrap/dist/css/bootstrap.css"
+];
+
+var nodeJs = [
+    // bootstrap
+    paths.nodeModules + "/bootstrap/dist/js/bootstrap.js",
+    // jquery
+    paths.nodeModules + "/jquery/dist/jquery.js",
+];
 
 paths.js = paths.webroot + "js/**/*.js";
 paths.minJs = paths.webroot + "js/**/*.min.js";
 paths.css = paths.webroot + "css/**/*.css";
 paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.concatJsDest = paths.webroot + "js/app.min.js";
+paths.concatCssDest = paths.webroot + "css/app.min.css";
 
 gulp.task("clean:js", function (cb) {
     rimraf(paths.concatJsDest, cb);
@@ -29,14 +42,14 @@ gulp.task("clean:css", function (cb) {
 gulp.task("clean", ["clean:js", "clean:css"]);
 
 gulp.task("min:js", function () {
-    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+    return gulp.src(nodeJs.concat([paths.js, "!" + paths.minJs]), { base: "." })
         .pipe(concat(paths.concatJsDest))
         .pipe(uglify())
         .pipe(gulp.dest("."));
 });
 
 gulp.task("min:css", function () {
-    return gulp.src([paths.css, "!" + paths.minCss])
+    return gulp.src(nodeCss.concat([paths.css, "!" + paths.minCss]))
         .pipe(concat(paths.concatCssDest))
         .pipe(cssmin())
         .pipe(gulp.dest("."));
