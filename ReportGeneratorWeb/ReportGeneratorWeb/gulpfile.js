a421fb9d3a9755df62da39cefcf97afae2cbb051
Ushakov Michael <um.nix.user@gmail.com>
2022-05-05 23:49:32 +0500

    gulp was upgraded to version 4 syntax

--- ReportGeneratorWeb/ReportGeneratorWeb/gulpfile.js
+++ ReportGeneratorWeb/ReportGeneratorWeb/gulpfile.js
@@ -1,20 +1,33 @@
 ﻿/// <binding BeforeBuild='min' Clean='clean' />
-"use strict";
+/* This is a settings of build tool - gulp, previous version was 3.x, recently (2022) we switched tp gulp 4
+ * https://gulpjs.com/docs/en/getting-started/javascript-and-gulpfiles/
+ * https://webdesign-master.ru/blog/tools/gulp-4-lesson.html
+ * to build using gulp install gulp && gulp-cli globally
+ * 1. npm install gulp -g && npm install gulp-cli -g (do this only ONCE)
+ * 2. npm install
+ * 3. gulp build
+ * As a result we got 2 files: wwwroot/css/app,min.css and wwwroot/js/app.min.js
+ */
 
-var gulp = require("gulp"),
-    rimraf = require("rimraf"),
-    concat = require("gulp-concat"),
-    cssmin = require("gulp-cssmin"),
-    uglify = require("gulp-uglify");
+//"use strict";
+
+const gulp = require("gulp");
+const rimraf = require("rimraf");
+const concat = require("gulp-concat");
+const cssmin = require("gulp-cssmin");
+const uglify = require("gulp-uglify");
+
+const { parallel, series, watch } = gulp;
+
 
 var paths = {
     webroot: "./wwwroot/",
-    nodeModules: "./node_modules"
+    nodeModules: "./node_modules/"
 };
 
 var nodeCss = [
     // bootstrap
-    paths.nodeModules + "/bootstrap/dist/css/bootstrap.css",
+    paths.nodeModules + "bootstrap/dist/css/bootstrap.css",
     // font awesome
     paths.nodeModules + "font-awesome-5-css/css/all.css",
     // report manager stylesheets
@@ -37,28 +50,38 @@ paths.minCss = paths.webroot + "css/**/*.min.css";
 paths.concatJsDest = paths.webroot + "js/app.min.js";
 paths.concatCssDest = paths.webroot + "css/app.min.css";
 
-gulp.task("clean:js", function (cb) {
+function cleanJs (cb) {
     rimraf(paths.concatJsDest, cb);
-});
+};
 
-gulp.task("clean:css", function (cb) {
+function cleanCss (cb) {
     rimraf(paths.concatCssDest, cb);
-});
+};
 
-gulp.task("clean", ["clean:js", "clean:css"]);
+function clean(cb) {
+    cleanJs(cb);
+    cleanCss(cb);
+    cb();
+};
 
-gulp.task("min:js", function () {
+function buildMinJs(cb) {
     return gulp.src(nodeJs.concat([paths.js, "!" + paths.minJs]), { base: "." })
         .pipe(concat(paths.concatJsDest))
         .pipe(uglify())
         .pipe(gulp.dest("."));
-});
+}
 
-gulp.task("min:css", function () {
+function buildMinCss(cb) {
     return gulp.src(nodeCss.concat([paths.css, "!" + paths.minCss]))
         .pipe(concat(paths.concatCssDest))
         .pipe(cssmin())
         .pipe(gulp.dest("."));
-});
+}
 
-gulp.task("min", ["min:js", "min:css"]);
+function build(cb) {
+    buildMinJs(cb);
+    buildMinCss(cb);
+    cb();
+}
+
+exports.build = series(clean, build);
